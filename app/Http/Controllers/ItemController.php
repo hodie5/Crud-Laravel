1d0ba1592988595ed75c1b48230cb269803ca21b
hodie <aloysiushodie5@gmail.com>
2020-07-03 10:17:37 +0700

    first commit

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ItemModel;
+class ItemController extends Controller
+{
+    //
+    public function index(){
+        $items = ItemModel::get_all();
+        return view('item.index', compact('items'));
+    }
+    public function create(){
+        return view('item.form');
+    }
+}
+
+class PertanyaanController extends Controller
+{
+    //
+    public function index(){
+        $items = ItemModel::get_all();
+        return view('item.index', compact('items'));
+    }
+    public function create(){
+        return view('item.form');
+    }
+}
---
+++ database/migrations/2020_07_02_201234_create_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigincrements('id');
+            $table->string('nama');
+            $table->string('description')->nullable(); //notnullable
+            $table->integer('stock')->nullable(); //notnullable
+            $table->integer('price')->nullable(); //notnullable
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
